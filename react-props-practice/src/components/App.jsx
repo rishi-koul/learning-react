2548efb07518be65e8fe2cd6d043d0b1f4c7405c
rishi-koul <rishikoul2@gmail.com>
2021-08-06 14:18:43 +0530

    mapping data to components

--- react-props-practice/src/components/App.jsx
+++ react-props-practice/src/components/App.jsx
@@ -7,17 +7,17 @@ function App() {
     <div>
       <h1 className="heading">My Contacts</h1>
       <Card name={contacts[0].name}
-    imgURL={contacts[0].imgURL} 
+    img={contacts[0].imgURL} 
     num={contacts[0].phone}
      email={contacts[0].email}/>
 
     <Card name={contacts[1].name}
-    imgURL={contacts[1].imgURL} 
+    img={contacts[1].imgURL} 
     num={contacts[1].phone}
      email={contacts[1].email}/>
 
     <Card name={contacts[2].name}
-    imgURL={contacts[2].imgURL} 
+    img={contacts[2].imgURL} 
     num={contacts[2].phone}
      email={contacts[2].email}/>
     </div>
